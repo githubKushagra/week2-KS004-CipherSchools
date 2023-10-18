fe640099163a0c93eeea36df9c1669073021ef8c
Kushagra Varshney <118505260+githubKushagra@users.noreply.github.com>
2023-10-18 18:57:42 +0530

    memory.cpp

---
+++ Lecture11 I-CipherSchools/memory.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "The size of int is: " << sizeof(int) << endl; // bytes
+    cout << "The size of long long int is: " << sizeof(long long int);
+
+    return 0;
+}
